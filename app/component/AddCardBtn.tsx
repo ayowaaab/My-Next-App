d8216631456b68a330fee715878cc1ff9ff466ce
ayowaaab <ayoub.dahmen@esprit.tn>
2023-12-18 10:23:03 +0100

    first commit

---
+++ app/component/AddCardBtn.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { Button } from "@radix-ui/themes";
+
+const AddCardBtn = () => {
+  return <Button onClick={() => console.log("hello")}>Click Me</Button>;
+};
+
+export default AddCardBtn;
